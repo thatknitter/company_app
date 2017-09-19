698b6eb4fabd616a3a11e5092da1ed77925fc635
Kimberly Lewis <kblewis.flt@gmail.com>
2017-09-19 11:28:43 -0600

    add full title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,25 +4,25 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | People Smart Web Solutions Sandbox"
+    assert_select "title", "People Smart Web Solutions Rails Sandbox"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | People Smart Web Solutions Sandbox"
+    assert_select "title", "Help | People Smart Web Solutions Rails Sandbox"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | People Smart Web Solutions Sandbox"
+    assert_select "title", "About | People Smart Web Solutions Rails Sandbox"
   end
 
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
-    assert_select "title", "Contact | People Smart Web Solutions Sandbox"
+    assert_select "title", "Contact | People Smart Web Solutions Rails Sandbox"
   end
 
 end
