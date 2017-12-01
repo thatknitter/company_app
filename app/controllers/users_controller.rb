67f3e1ad4ec8cb66fa8905c5dceca289da81e105
Kimberly Lewis <kblewis.flt@gmail.com>
2017-11-30 17:45:53 -0700

    testing for final error integration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #handle a successful save
+      flash[:success] = "Welcome to the Sandbox!"
+      redirect_to @user
     else
       render 'new'
     end
