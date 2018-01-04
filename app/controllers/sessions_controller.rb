547e97faeabaff23648bb0097316f2fcd6561155
Kimberly Lewis <kblewis.flt@gmail.com>
2018-01-04 11:47:08 -0700

    sessions controller untested

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #Log the user in and redirect to the user's show page
+    else
+      flash[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 
   def destroy
-    
+
   end
 end
